b54e343859c0f71e9252244e5e771280e383ebab
bin386@163.com <bin386@163.com>
2020-05-02 20:32:15 +0800

    JSONP跨域，前端适配，后台配合，前后台同时改造，新增jsonp插件依赖，App.vue，跨域接口jsonp js请求测试，返回数据页面绑定渲染测试成功

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
+// import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
@@ -16,13 +17,19 @@ export default {
   },
   data(){
     return{
-
+      data:''
     }
   },
   mounted() {
-    let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
-    axios.get(url).then((res)=>{
-      console.log(res);
+    // let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
+    let url = "https://www.imooc.com/activity/newcomer";
+    // axios.get(url).then((res)=>{
+    //   console.log(res);
+    // })
+    jsonp(url,{},(err,res)=>{
+      let result = res;
+      this.data = result;
+      console.log(this.data);
     })
   },
 }
