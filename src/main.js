74a4a0f40a06a33513561dac430880e3796ac1e8
bin386@163.com <bin386@163.com>
2020-05-03 15:38:34 +0800

    main.js增加axios.defaults.baseURL,timeout，及错误拦截，axios.interceptors.response

--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios' // 将作用域对象挂载到vue实例上，方便用this调用
 import App from './App.vue'
 
+// jsonp，cros当后端域名与前端不一样时候需全写，eg：'http:www.baidu.com'
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api'; // 接口代理/a/b : /api/a/b => /a/b：当前接口域名和前端域名一样可简写
+axios.defaults.timeout = 8000; // 请求超时时间，提升用户体验
+
+// 接口错误拦截(状态码需前后台根据业务协定,eg:0-成功，10-未登录)
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status === 0){
+    return res.data;
+  }else if(res.status === 10){
+    // 无法使用路由进行跳转，路由挂载在vue实例，App.vue及每个页面才能使用this.$route.push,取不到this
+    window.location.href='/#/login'; 
+  }else{
+    alert(res.msg)
+  }
+})
+
+// 请求拦截(后台管理系统，表单查询，参数较多)
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// 加载插件
+Vue.use(VueAxios,axios);
+// 默认非生产环境
 Vue.config.productionTip = false
 
 new Vue({
