c5ceb08716e6f39067626fbdb1c0e6d4efaedef4
bin386@163.com <bin386@163.com>
2020-05-05 14:05:59 +0800

    首页手机产品列表部分功能接口接通，增加hover效果

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -74,14 +74,14 @@
           <div class="list-box">
             <div class="list" v-for="(arr,i) in phoneList" :key="i">
               <div class="item" v-for="(item,j) in arr" :key="j">
-                <span>新品</span>
+                <span :class="{'new-pro':j%2===0}">新品</span>
                 <div class="item-img">
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6f2493e6c6fe8e2485c407e5d75e3651.jpg" alt="">
+                  <img :src="item.mainImage" alt="">
                 </div>
                 <div class="item-info">
-                  <h3>小米9</h3>
-                  <p>骁龙855，索尼4800万超广角微距</p>
-                  <p class="price">2999元</p>
+                  <h3>{{item.name}}</h3>
+                  <p>{{item.subtitle}}</p>
+                  <p class="price">{{item.price}}元</p>
                 </div>
               </div>
             </div>
@@ -104,7 +104,7 @@ export default {
     ServiceBar
   },
   data() {
-      return {
+    return {
         swiperOption: {
           autoplay:true,
           loop:true,
@@ -193,12 +193,27 @@ export default {
             img:'/imgs/ads/ads-4.jpg'
           }
         ],
-        phoneList:[
-          [1,1,1,1],
-          [1,1,1,1]
-        ]
-      }
-    },
+        phoneList:[]
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init(){
+      this.axios.get('/products',{
+        params:{
+          categoryId:100012,
+          pageSize:8
+        }
+      }).then((res)=>{
+        console.log(res);
+        let {list} = res;
+        this.phoneList = [list.slice(0,4),list.slice(4,8)];
+        console.log(this.phoneList);
+      })
+    }
+  },
 }
 </script>
 <style lang="scss">
@@ -327,11 +342,24 @@ export default {
               height:302px;
               background-color: $colorG;
               text-align: center;
+              transition: all .2s linear;
               span{
-
+                display: inline-block;
+                width:67px;
+                height:24px;
+                font-size:14px;
+                line-height: 24px;
+                color:$colorG;
+                &.new-pro{
+                  background-color: #7ECF68;
+                }
+                &.kill-pro{
+                  background-color: #E82626;
+                }
               }
               .item-img{
                 img{
+                  width:100%;
                   height:195px;
                 }
               }
@@ -359,6 +387,11 @@ export default {
                     vertical-align: middle;
                   }
                 }
+              }
+              &:hover{
+                box-shadow: 0 15px 30px rgba(0,0,0,.1);
+                transform: translate3d(0,-2px,0);
+                transition: all .2s linear;
               }
             }
           }
